7f34de087c8d898cdd2ce5e802fdc529b5a01a21
MartinicaMat <mgreshfreguin@gmail.com>
2026-07-08 13:14:04 -0400

    Update minicraft launcher.bat

--- Minicraft Launcher/minicraft launcher.bat
+++ Minicraft Launcher/minicraft launcher.bat
@@ -12,7 +12,7 @@ set /p choix=Selectionne une option (1-3) :
 
 echo ^G
 
-if %choix%==1 java -cp ld22.jar com.mojang.ld22.Game
+if %choix%==1 goto lancer
 if %choix%==2 goto credits
 if %choix%==3 start https://github.com/MartinicaMat/Minicraft-Launcher
 
@@ -24,3 +24,10 @@ echo.
 echo CREATEUR PRINCIPAL : MartinicaMat
 echo DEVLOPPEUR : MartinicaMat
 echo SITE WEB DU DEVLOPPEUR : martinicamat.github.io
+
+:lancer
+if not exist ld22.jar (
+    echo Le fichier ld22.jar est manquant. Téléchargement en cours...
+    start https://s3.amazonaws.com/ld48/ld22/ld22.jar
+)
+java -cp ld22.jar com.mojang.ld22.Game
